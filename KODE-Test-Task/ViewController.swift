0ba762a3fb3061b90f785f2476710a361af25fef
semaDilthey <gaidsemen@yandex.ru>
2023-11-21 18:48:10 +0300

    Initial Commit

---
+++ KODE-Test-Task/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KODE-Test-Task
+//
+//  Created by Семен Гайдамакин on 21.11.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
